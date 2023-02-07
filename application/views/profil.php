fcf30acc7b2e137f2571c96d9aa3afa326572c9f
projetETU <124677040+Mendrika123@users.noreply.github.com>
2023-02-08 00:47:08 +0300

    Update profil.php

--- application/views/profil.php
+++ application/views/profil.php
@@ -80,7 +80,7 @@ https://templatemo.com/tm-559-zay-shop
                             <a class="nav-link" href="index.html">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.html">About</a>
+                            <a class="nav-link" href="me.html">Mon profil</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="shop.html">Shop</a>
